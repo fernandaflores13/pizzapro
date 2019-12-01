435156fe09f8b332604e06ec395919efee7deb6c
Fernanda Flores <ffloreslugo@gmail.com>
2019-12-01 00:17:30 -0700

    modificaciones en modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,40 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 // RUTAS
+import { APP_ROUTES } from './app.routes';
 
-
-
+// MODULOS
+import { PagesModule } from './pages/pages.module';
+// COMPONENTEs
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './share/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { HeaderComponent } from './share/header/header.component';
-import { SidebarComponent } from './share/sidebar/sidebar.component';
-import { BreadcrumsComponent } from './share/breadcrums/breadcrums.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumsComponent,
-    ProgressComponent,
-    PagesComponent,
-    Graficas1Component,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
